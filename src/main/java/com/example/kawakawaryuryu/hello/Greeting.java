d43110243589759611d0b7df57ccaa08d2137bf2
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2018-06-02 18:04:48 +0900

    add GrettingController

---
+++ src/main/java/com/example/kawakawaryuryu/hello/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.kawakawaryuryu.hello;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Greeting {
+
+    private int id;
+    private String content;
+}
---
+++ src/main/java/com/example/kawakawaryuryu/hello/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.kawakawaryuryu.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+public class GreetingController {
+
+    private final String template = "Hello, %s";
+    private final AtomicInteger counter = new AtomicInteger();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
